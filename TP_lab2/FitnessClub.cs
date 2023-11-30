49d873fce77f081b2a454575666cff00a689fd47
Lilia Bituk <lilia.bituk@bk.ru>
2023-11-30 18:08:55 +0700

    убрала вызов ридеров в конструкторе класса FitnessClub

--- TP_lab2/FitnessClub.cs
+++ TP_lab2/FitnessClub.cs
@@ -12,46 +12,6 @@ namespace TP_lab2
 
         public List<Massage> massageList = new List<Massage>(); 
 
-        public FitnessClub(string tariffsFilePath,
-                           string extraServicesFilePath,
-                           string[] groupTrainingFilePaths,
-                           string[] massageFilePaths)
-        {
-            // read info about tariffs
-            TariffsReader readerTariffs = new TariffsReader(tariffsFilePath);
-
-            if (readerTariffs.TariffsInfo.Count > 0)
-            {
-                var info = readerTariffs.TariffsInfo[0];
-                tariffs = info.tariffs;
-                months = info.months;
-            }
-
-            // read info about extra services
-            ExtraServicesReader readerExtraServices = new ExtraServicesReader(extraServicesFilePath);
-            if (readerExtraServices.ExtraServicesInfo.Count > 0)
-            {
-                var info = readerExtraServices.ExtraServicesInfo[0];
-                ExtraServices = info.ExtraServices;
-            }
-
-            // read info about group trainings
-            foreach (string file in groupTrainingFilePaths)
-            {
-                GroupTrainingReader reader = new GroupTrainingReader();
-                GroupTraining groupTrainig = reader.Read(file);
-                groupTrainingList.Add(groupTrainig);
-            }
-
-            // read info about massages
-            foreach (string file in massageFilePaths)
-            {
-                MassageReader readerMassage = new MassageReader();
-                Massage massage = readerMassage.Read(file);
-                massageList.Add(massage);
-            }
-        }
-
         public bool GroupTrainingsAreAvaliable(string selectedTariff)
         {
             return ExtraServices[selectedTariff][0];
@@ -60,23 +20,6 @@ namespace TP_lab2
         public bool MassageIsAvaliable(string selectedTariff)
         {
             return ExtraServices[selectedTariff][1];
-        }
-
-        public void TypeTicketToFile(TariffSelectedByUser tariff,
-                                    GroupTrainingSelectedByUser selectedGroupTrainingObject,
-                                    MassageSelectedByUser massage)
-        {
-            using (StreamWriter sw = new StreamWriter("ticket.txt", false))
-            {
-                sw.Write($"Тариф: {tariff.typeOfTariff}\n" +
-                        $"Абонемент на {tariff.durationOfTariff} мес\n" +
-                        $"Стоимость: {tariff.priseOfTariff} руб\n" +
-                        $"Тип тренировки: {selectedGroupTrainingObject.subtype}\n" +
-                        $"Время тренировки: {selectedGroupTrainingObject.time}\n" +
-                        $"Массаж: {massage.Type}\n" +
-                        $"Массажист: {massage.Master}\n" +
-                        $"Время массажа: {massage.Time}");
-            }
         }
     }
 }
--- TP_lab2/Program.cs
+++ TP_lab2/Program.cs
@@ -7,7 +7,42 @@ internal class Program
         string[] groupTrainingFilePaths = new string[] { args[3], args[4], args[5], args[6], args[7] };
         string[] massageFilePaths = new string[] { args[8], args[9], args[10] };
 
-        FitnessClub fitnessClub = new FitnessClub(args[0], args[1], groupTrainingFilePaths, massageFilePaths);
+        FitnessClub fitnessClub = new FitnessClub();
+
+        // read info about tariffs
+        TariffsReader readerTariffs = new TariffsReader(args[0]);
+
+        if (readerTariffs.TariffsInfo.Count > 0)
+        {
+            var info = readerTariffs.TariffsInfo[0];
+            fitnessClub.tariffs = info.tariffs;
+            fitnessClub.months = info.months;
+        }
+
+        // read info about extra services
+        ExtraServicesReader readerExtraServices = new ExtraServicesReader(args[1]);
+        if (readerExtraServices.ExtraServicesInfo.Count > 0)
+        {
+            var info = readerExtraServices.ExtraServicesInfo[0];
+            fitnessClub.ExtraServices = info.ExtraServices;
+        }
+
+        // read info about group trainings
+        foreach (string file in groupTrainingFilePaths)
+        {
+            GroupTrainingReader reader = new GroupTrainingReader();
+            GroupTraining groupTrainig = reader.Read(file);
+            fitnessClub.groupTrainingList.Add(groupTrainig);
+        }
+
+        // read info about massages
+        foreach (string file in massageFilePaths)
+        {
+            MassageReader readerMassage = new MassageReader();
+            Massage massage = readerMassage.Read(file);
+            fitnessClub.massageList.Add(massage);
+        }
+
         TariffsUserInteraction tariffUserInteraction = new TariffsUserInteraction(fitnessClub.tariffs, fitnessClub.months);
         TariffSelectedByUser tariffObject = new TariffSelectedByUser();
 
@@ -46,7 +81,8 @@ internal class Program
         }
 
         // Генерация билета из собранных данных
-        fitnessClub.TypeTicketToFile(tariffObject, selectedGroupTrainingObject, massageObject);
+        TicketGenerator ticket = new TicketGenerator();
+        ticket.TypeTicketToFile(tariffObject, selectedGroupTrainingObject, massageObject);
         Console.WriteLine("Спасибо за покупку! Чек отправлен на почту.");
     }
 }
